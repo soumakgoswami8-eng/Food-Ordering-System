93d9048159af5eac200a47bb765cc49834d5f511
soumakgoswami8-eng <soumakgoswami8@gmail.com>
2025-11-13 18:38:43 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+  <?php include('partials-front/menu.php'); ?>
+
+  <!-- Contact Section -->
+<div class="contact-section">
+  <div class="contact-box">
+    <h2>Contact Information</h2>
+    <p>We’d love to hear from you! You can reach us using the details below.</p>
+
+    <div class="contact-details">
+      <p><strong>📍 Address:</strong> Techno Main, Salt Lake , Kolkata, India</p>
+      <p><strong>📞 Phone:</strong> ‪+91 9407969697‬</p>
+      <p><strong>📧 Email:</strong> soumakgoswami8@gmail.com</p>
+      <p><strong>🌐 Website:</strong>https://github.com/soumakgoswami8-eng</p>
+    </div>
+  </div>
+</div>
+
+
+
+  <?php include('partials-front/footer.php'); ?>
+
